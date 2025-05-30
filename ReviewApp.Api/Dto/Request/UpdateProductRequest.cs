1a00a995eaefd36036949ed0ddedf08f259ff901
Mihaly Varga <mihaly.varga@elpro.com>
2025-05-30 22:40:36 +0200

    fix(api): require description in product updates

--- ReviewApp.Api/Dto/Request/UpdateProductRequest.cs
+++ ReviewApp.Api/Dto/Request/UpdateProductRequest.cs
@@ -4,6 +4,7 @@ namespace ReviewApp.Api.Dto.Request
 {
     public class UpdateProductRequest
     {
+        [Required]
         [StringLength(500, MinimumLength = 1)]
         public string Description { get; set; } = null!;
     }
